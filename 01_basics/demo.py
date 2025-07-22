407b55690125927855e245ef40c5f219fd058e34
cHarish45 <chamarthiharishraj@gmail.com>
2025-07-22 17:45:16 +0530

    notes.txt

---
+++ 01_basics/demo.py
@@ -0,0 +1,27 @@
+"""import keyword
+print(keyword.kwlist)"""
+
+"""import math
+print(sqrt(16))
+
+from math import sqrt
+print(sqrt(16))"""
+
+"""a=10
+print(type(a))
+print(id(10))"""
+
+#The data differnce between normal data and complex data, "Mostly Ids storage different in both".
+"""list_1 = [1,2,3]
+list_2 = [1,2,3]
+print(id(list_1))
+print(id(list_2))"""
+
+"""a=10
+b="Awesome"
+print(a,b)"""
+
+"""x=y=z="one"
+print(x)
+print(y)
+print(z)"""
---
+++ 01_basics/interpolation.py
@@ -0,0 +1,9 @@
+name = "Harish"
+age = 21
+new_age = 9
+
+"""print("My name is "+name+"I'm "+age+" years old." ) #String Concatenation
+TypeError: can only concatenate str (not "int") to str"""
+
+print(f"My Name is {name} I'm {age} years old") #Interpolation Method
+print(f"My Name is {name} I'm {age+new_age} years old") #Interpolation Method using Concatenation also like inside part.
