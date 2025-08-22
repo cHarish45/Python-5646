d5faa5a2a5178aa9abfe6e12700d9073585b555c
cHarish45 <chamarthiharishraj@gmail.com>
2025-08-22 11:20:10 +0530

    11_modules

---
+++ 11_modules/client-1.py
@@ -0,0 +1,9 @@
+# Client want to use my_module 
+import my_module
+
+name = "Lync"
+email = "info@lync.com"
+
+# print(my_module.email) # getting variable
+print(my_module.mul(10,20)) # getting functions
+print(my_module.profile_info(name="Edu",email="info@edu.com"))
